c4f908140d59cd659c809216192b7bb24cdf37f9
Jasuquo <joemileasuquo@gmail.com>
2022-08-02 14:46:36 +0100

    Update lovecalculator.py

--- lovecalculator.py
+++ lovecalculator.py
@@ -1,10 +1,8 @@
-# 🚨 Don't change the code below 👇
+# 🚨capture user input below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 
 joined_names = name1 + name2 
 
