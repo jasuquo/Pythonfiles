452419d51914854f45a6ae6ad8b3d651577fbf1c
Jasuquo <joemileasuquo@gmail.com>
2022-08-02 20:01:21 +0100

    Update and rename treasure map to treasuremap.py

--- treasuremap.py
+++ treasuremap.py
@@ -4,9 +4,8 @@ row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
+#there are 3columns and 3rows input should in this format e.g 23 column 2 row 3
 position = input("Where do you want to put the treasure? ")
-
-#define rows and colums 👇
 
 horizontal = int(position[0])
 vertical = int(position[1])
